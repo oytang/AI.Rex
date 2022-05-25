5275ebaf0d0b339f93cb218cef1c5dd92c3aa6b0
Chemino <yifengt@uchicago.edu>
2022-05-25 13:46:52 -0500

    init commit

---
+++ data/README.md
@@ -0,0 +1,13 @@
+# data of chemical reactions
+
+- `cleaned_uspto50k.csv` 包括了由USPTO数据集清洗采样得到的一定数量的可性反应，即正例（来源：https://github.com/Hanjun-Dai/GLN）
+- `negative_strict.csv` 第一类负例 通过将一个反应的反应模板在反应物上进行错位匹配生成 代表了立体/区位选择性错误的反应
+- `negative_random.csv` 第二类负例 通过将一个反应与其他反应的模板进行匹配生成 代表了反应物发生反应的更多可能 但实际的产物是动力学/热力学最优的产物 可以认为其它产物不足以生成 生成时对一个反应随机筛选了一定数量的模板与之进行匹配
+- `negative_random_comprehensive.csv` 可通过compiler来进行整合生成 使用方法如下 该文件也是包含第二类负例 唯一不同在于对于每一个反应遍历了所有USPTO-50k数据集中的模板 因此是该数据集基础上可以生成的最完备的负例数据集
+    ```
+    python compile_random_comprehensive.py
+    ```
+- `ECFP-split.py` 可以根据反应物的分子指纹ECFP来将反应进行聚类然后进行train-valid-test-split 同时也做了更多一些预处理来最大限度上避免数据泄露 使用方法如下
+    ```
+    python ECFP-split.py [--help] [--num_false 42000 #从整个随机负例中采样的数量] [--train_size 0.7] [--valid_size 0.2] [--test_size 0.1] [--ECFP4nBits 1024 #分子指纹二元向量的长度 # 参数为0时禁用基于ECFP的分割而采用随机分割] [--QM_compatible False 是否用与QM描述符预计算兼容的负例子集(会覆盖--num_false参数)]
+    ```
---
+++ data/utils.py
@@ -0,0 +1,58 @@
+# Basic workflow credit to qianlong's script
+# The codes are comestically decorated
+# with some necessary bug fix
+
+from rdkit.Chem import MolToSmiles, MolFromSmiles, rdChemReactions
+
+
+def reverse_temp(temp):
+    """
+    Convert retro template to forward template;
+    This was re-implemented, for
+    (1) handling the exception where there are more than one molecule as product;
+    (2) better readability;
+    (3) better compatibility with `rdc.rdchiralRunText`
+    """
+    pt, rts = temp.split(">>")
+    return rts.replace(').(', '.') + '>>' + pt.replace(').(', '.')
+
+
+def rxn2rtpt(rxn_smi):
+    """
+    Convert reaction SMILES into reactants and products
+    in a way that is compatible with `rdc.rdchiralRunText`
+    and not discarding meaningful data
+    """
+    # for normal reactions
+    if rxn_smi.count('>>') == 1:
+        return rxn_smi.split('>>')
+
+    # for reactions with one reactant in middle
+    # like 'Cc1ccc(C(=O)O)cc1F>O=C1CCC(=O)N1Br>O=C(O)c1ccc(CBr)c(F)c1'
+    elif rxn_smi.count('>') == 2:
+        rt1, rt2, pt = rxn_smi.split('>')
+        # there are possible cases without rt1
+        # like '>O=C(O)C1CCN(C(=O)CO)CC1>O'
+        if rt1 == '':
+            return rt2, pt
+        else:
+            return f'{rt1}.{rt2}', pt
+    
+    # Unknown situation, might happen in uncleaned dataset
+    raise ValueError(f'Reaction SMILES is not valid: {rxn_smi}')
+
+
+def can_SMILES(smiles):
+    """
+    Convert a SMILES in any format to a canonicalized SMILES.
+    """
+    return MolToSmiles(MolFromSmiles(smiles))
+
+
+def removemap(rxnstr):
+    """
+    Remove atom-mapping
+    """
+    rxn = rdChemReactions.ReactionFromSmarts(rxnstr, useSmiles=True)
+    rdChemReactions.RemoveMappingNumbersFromReactions(rxn)
+    return rdChemReactions.ReactionToSmiles(rxn)
